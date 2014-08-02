9659e61cbff057c0d916cd26de8bd980ecc6ecad
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-08-02 15:42:21 -0400

    fix indenting and formatting

--- spritz.js
+++ spritz.js
@@ -8,13 +8,13 @@ var readability_token = '172b057cd7cfccf27b60a36f16b1acde12783893';
 var diffbot_token = '2efef432c72b5a923408e04353c39a7c';
 
 function create_spritz(){
-
-     spritz_loader = function() {
+    spritz_loader = function() {
         //getURL("https://rawgithub.com/Miserlou/OpenSpritz/master/spritz.html", function(data){
 
         //getURL("https://rawgithub.com/Miserlou/OpenSpritz/dev/spritz.html", function(data){
 
-        // This won't work in Firefox because an old bug and won't work in Chrome because of security stuff:
+        // This won't work in Firefox because an old bug and won't work in
+        // Chrome because of security stuff:
         //getURL("spritz.html", function(data){
 
         //getURL("https://rawgithub.com/Miserlou/OpenSpritz/dev/spritz.html", function(data){
